2b62029d6777d15f9224de980d020bb188fda2c4
Avery <44121063+Avery007@users.noreply.github.com>
2021-03-16 20:06:36 -0400

    add

--- Assets/mouseOver.cs
+++ Assets/mouseOver.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class mouseOver : MonoBehaviour
+
 {
+    public GameObject target;
+    void OnMouseDown()
+    {
+        Debug.Log("detection");
+        // Destroy the gameObject after clicking on it
+        Destroy(target); 
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
