e8ad7a04f71e66153462a9783d3b2e6a27b1a6e9
Avery <44121063+Avery007@users.noreply.github.com>
2021-03-17 12:28:40 -0400

    final

--- Assets/mouseOver.cs
+++ Assets/mouseOver.cs
@@ -6,11 +6,22 @@ public class mouseOver : MonoBehaviour
 
 {
     public GameObject target;
+    public GameObject wall;
+    public UnityEngine.UI.Text text;
+    private static int number = 0; 
     void OnMouseDown()
     {
         Debug.Log("detection");
         // Destroy the gameObject after clicking on it
-        Destroy(target); 
+        Destroy(target);
+        number = number + 1;
+        text.text = "balls collected: " + number;
+        Debug.Log(number);
+        if(number==4)
+        {
+            Destroy(wall);
+            text.text = "Good job you got all of them! Now move to exit";
+        }
     }
 
 
